dea870592cf5bbdb053d8a6061e4de814ea29aca
dhiili <narayanavaramdhilli7461.com>
2023-04-04 15:04:24 +0530

    first commit

---
+++ app4/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def sachin(request):
+    return render(request,'sachin.html')
---
+++ project16/urls.py
@@ -0,0 +1,36 @@
+"""project16 URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from app1.views import *
+from app2.views import *
+from app3.views import *
+from app4.views import *
+from app5.views import *
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    
+    path('dhoni/',dhoni,name='dhoni'),
+    path('virat/',virat,name='virat'),
+    
+    path('rohit/',rohit,name='rohit'),
+    
+    path('sachin/',sachin,name='sachin'),
+    
+    path('home/',home,name='home'),
+    
+]
